aa42a5043645f046f1d8931aafcae3e95aad751e
Rast <rastorgueff28@mail.ru>
2018-11-11 21:22:41 +0300

    ffter 3d video

---
+++ src/main/java/com/testTask/Voter/controllers/RegistrationController.java
@@ -0,0 +1,34 @@
+package com.testTask.Voter.controllers;
+
+import com.testTask.Voter.domain.Role;
+import com.testTask.Voter.domain.User;
+import com.testTask.Voter.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private UserRepo userRepo;
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+    @PostMapping("/registration")
+    public String addUser(User user,Map<String,Object> model){
+      User userFromDb = userRepo.findByUsername(user.getUsername());
+      if(userFromDb != null){
+          model.put("message","User exists!");
+          return "registration";
+                }
+                user.setActive(true);
+      user.setRoles(Collections.singleton(Role.USER));
+      userRepo.save(user);
+        return "redirect:/login";
+    }
+}
---
+++ src/main/java/com/testTask/Voter/domain/Role.java
@@ -0,0 +1,5 @@
+package com.testTask.Voter.domain;
+
+public enum Role {
+    USER;
+}
